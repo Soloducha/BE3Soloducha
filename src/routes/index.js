352ed6cbb2b87617d27b26da1dee867bd37ef59c
Leandro Soloducha <soloducha@gmail.com>
2026-07-22 10:15:28 -0300

    se corrige src/routes/index.js para que use el router correctamente

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,14 @@
 import { Router } from "express";
-import express from "express";
 import usersRouter from "./users.routes.js";
 import ordersRouter from "./orders.routes.js";
 import deliveriesRouter from "./deliveries.routes.js";
 import productsRouter from "./products.routes.js";
 
 const router = Router();
-const app = express();
-app.use("/users", usersRouter);
-app.use("/orders", ordersRouter);
-app.use("/deliveries", deliveriesRouter);
-app.use("/products", productsRouter);
+
+router.use("/users", usersRouter);
+router.use("/orders", ordersRouter);
+router.use("/deliveries", deliveriesRouter);
+router.use("/products", productsRouter);
 
 export default router;
